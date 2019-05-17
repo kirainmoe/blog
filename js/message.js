9a7c5ad208ef5aed469978649ea74179ad75d8d6
kirainmoe <i@kirainmoe.com>
2019-05-17 13:05:14 +0800

    change

---
+++ js/message.js
@@ -0,0 +1,12 @@
+
+function initLive2d (){
+    $('.hide-button').fadeOut(0).on('click', () => {
+        $('#landlord').css('display', 'none')
+    })
+    $('#landlord').hover(() => {
+        $('.hide-button').fadeIn(600)
+    }, () => {
+        $('.hide-button').fadeOut(600)
+    })
+}
+initLive2d ();
